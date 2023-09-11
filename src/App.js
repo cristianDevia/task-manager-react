82463c101295fa1b41cac0215a26bbf9611f483b
Cristian Devia <c.devia8@gmail.com>
2023-09-11 13:07:57 -0500

    add imports for todo components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import logo from './platzi.webp';
-import './App.css';
-
+import "./App.css";
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoItem } from "./components/TodoItem";
+import { TodoSearch } from "./components/TodoSearch";
+import { TodoList } from "./components/TodoList";
+import { CreateTodoButton } from "./components/CreateTodoButton";
 function App() {
   return (
     <div className="App">
-    <TodoItem/>
-    <TodoItem/>
-    <TodoItem/>
-    <TodoItem/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+      <CreateTodoButton />
     </div>
-  );
-}
-function TodoItem() {
-  return(
-    <li>
-      <span>V</span>
-      <p>Task 1</p>
-      <span>X</span>
-    </li>
   );
 }
 export default App;
