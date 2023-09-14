e128b42743ae175db4c0748cb94dff75106c33f7
Cristian Devia <c.devia8@gmail.com>
2023-09-14 15:06:53 -0500

    add text style correction

--- src/components/TodoCounter/TodoCounter.js
+++ src/components/TodoCounter/TodoCounter.js
@@ -1,5 +1,10 @@
 import "./TodoCounter.css";
 function TodoCounter({ total, completed }) {
-  return <h1>{`Has completado ${completed} de ${total} TODOS`}</h1>;
+  return (
+    <h1>
+      {`Has completado `} <span>{completed}</span> {`de `}
+      <span>{total}</span> {` TODOS`}
+    </h1>
+  );
 }
 export { TodoCounter };
