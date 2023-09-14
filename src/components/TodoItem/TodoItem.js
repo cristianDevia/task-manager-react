a9c2a0a93901059074485eae16c9e3841fbc5132
Cristian Devia <c.devia8@gmail.com>
2023-09-14 14:03:21 -0500

    add own todoItem styles

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,9 +1,16 @@
+import "./TodoItem.css";
+import check_icon from "../../assets/check_icon.svg";
+import trash_x from "../../assets/trash_x.svg";
 function TodoItem({ taskText, completed }) {
   return (
     <li>
-      <span>V</span>
+      <span>
+        <img src={check_icon} alt="check" />
+      </span>
       <p>{`${taskText}`}</p>
-      <span>X</span>
+      <span className="removeItem">
+        <img src={trash_x} alt="remove" />
+      </span>
     </li>
   );
 }
