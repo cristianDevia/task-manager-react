cb63d71fdd311489cb7a6a79de2870d726aa5936
Cristian Devia <c.devia8@gmail.com>
2023-09-14 18:31:25 -0500

    add new styles for createTodoButton

--- src/components/CreateTodoButton/CreateTodoButton.js
+++ src/components/CreateTodoButton/CreateTodoButton.js
@@ -1,12 +1,5 @@
 import "./CreateTodoButton.css";
-import add_button from "../../assets/add_button.svg";
 function CreateTodoButton() {
-  return (
-    <div>
-      <button id="addTask">
-        <img src={add_button} alt="" />
-      </button>
-    </div>
-  );
+  return <button id="addTask">+</button>;
 }
 export { CreateTodoButton };
